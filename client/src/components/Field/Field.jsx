3457e613af117c4866d4199c97cc26d78d0a80cd
Katarzyna Poreba <katarzynaporeba@icloud.com>
2022-11-20 08:25:35 +0100

    Add tasks component

--- client/src/components/Field/Field.jsx
+++ client/src/components/Field/Field.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 
-export function Field({ icon, fieldText }) {
+export function Field({ icon, fieldText, status }) {
   return (
-    <Box className="field-container">
+    <Box className={`field-container ${status}`}>
       <Typography className="field-icon">{icon}</Typography>
       <Typography className="field-description">{fieldText}</Typography>
     </Box>
--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -2,12 +2,26 @@ import "./Task.scss";
 import React from "react";
 import Box from "@mui/material/Box";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ScoreboardIcon from "@mui/icons-material/Scoreboard";
+import InvertColorsIcon from "@mui/icons-material/InvertColors";
+import { Typography } from "@mui/material";
 import Field from "../Field";
 
-export function Task() {
+export function Task({ src, title, description, points, savings, status }) {
   return (
-    <Box className="task-container">
-      <Field icon={<CheckCircleOutlineIcon />} fieldText="Ukończone" />
+    <Box className={`task-container ${status}`}>
+      <Box className="task-top-container ">
+        <Field icon={<CheckCircleOutlineIcon />} fieldText="Ukończone" status={status} />
+        <img src={src} alt="pic" />
+      </Box>
+      <Box className="task-text-container">
+        <Typography className="task-text-title">{title}</Typography>
+        <Typography className="task-text-description">{description}</Typography>
+      </Box>
+      <Box className="task-stats-container">
+        <Field icon={<ScoreboardIcon />} fieldText={points} status="none" />
+        <Field icon={<InvertColorsIcon />} fieldText={savings} status="none" />
+      </Box>
     </Box>
   );
 }
